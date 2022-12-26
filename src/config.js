c69e9c5297b609fd232f20e8a9324338fc36e09b
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2022-12-26 15:17:04 +0800

    Revert "Update config.js"
    This reverts commit 76355cadb9f93055692cff7052bd97916c438973.

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ const defaultConfig = {
     token: 'YOUR_GITHUB_ACCESS_TOKEN'
   },
   discord: {
-    enabled: true,
+    enabled: false,
     id: 'YOUR_DISCORD_WEBHOOK_ID',
     token: 'YOUR_DISCORD_WEBHOOK_TOKEN'
   },
